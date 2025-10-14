cb7cc8c68c1e4c22a7a1e91879e9e8067ad41337
janik <48893696+JanikThePanic@users.noreply.github.com>
2025-10-14 01:38:59 -0400

    reboil main

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-import App from "./App";
-import Projects from "./pages/Projects";
-import Knickknacks from "./pages/Knickknacks";
-import Resume from "./pages/Resume";
-import Contact from "./pages/Contact";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Root Pages */}
-        <Route path="/" element={<App />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/knickknacks" element={<Knickknacks />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<Contact />} />
-
-        {/* 404 */}
-        <Route path="*" element={<p>There's nothing here: 404!</p>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );

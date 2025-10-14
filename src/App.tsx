b540eec6795ef2078f9e7cc49c6a5e393e8f6103
janik <48893696+JanikThePanic@users.noreply.github.com>
2025-10-14 01:40:58 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "./global.css";
+
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Knickknacks from "./pages/Knickknacks";
